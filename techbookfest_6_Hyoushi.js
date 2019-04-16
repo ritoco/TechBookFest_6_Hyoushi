2d7c3b3cc5cc57c779489b03700f308b403a0416
ritoco <38111030+ritoco@users.noreply.github.com>
2019-04-17 01:50:57 +0900

    20190417_up
    update : Fine correction.

--- techbookfest_6_Hyoushi.js
+++ techbookfest_6_Hyoushi.js
@@ -6,8 +6,8 @@ function setup() {
   rectMode(CENTER);
   pixelDensity();
   noStroke();
-  colorMode(HSB,360,100,100,100);
-  background(170, 10,100);
+  colorMode(HSB,360,100,100);
+  background(170,10,100);
 
   // 描画する図形の配置をランダムにする
   spacer = random(60,100);
@@ -21,10 +21,10 @@ function draw() {
   for (let x = 0; x < width; x += spacer) {
     for (let y = 0; y < height; y += spacer) {
 
-	  //図形の色と透明度の指定
-		fill(random(170,210), 85 ,random(20,100),random(5,100));
+	  // 図形の色指定
+		  fill( random(170,210), 85 ,random(20,100) );
 
-      //縦横に図形を置いていく
+    // 縦横に図形を置いていく
       rect(x + spacer / 2, y + spacer / 2,50,50);
       rect(x + spacer / 2, y + spacer / 2,50,50);
     }
